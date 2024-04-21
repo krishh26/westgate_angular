0bf90e87cbc596bd57ea83dd4f6afee0e261357f
kishanivotion <kishan@ivotiontech.com>
2024-04-22 01:02:20 +0530

    changes done

--- src/app/modules/bos-user/boss-user-live-project-listing/boss-user-live-project-listing.component.ts
+++ src/app/modules/bos-user/boss-user-live-project-listing/boss-user-live-project-listing.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ProjectService } from 'src/app/services/project-service/project.service';
+import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
 
 @Component({
   selector: 'app-boss-user-live-project-listing',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./boss-user-live-project-listing.component.scss']
 })
 export class BossUserLiveProjectListingComponent {
+
+  showLoader: boolean = false;
+  projectList: any = [];
+
+  page: number = pagination.page;
+  pagesize = pagination.itemsPerPage;
+  totalRecords: number = pagination.totalRecords;
+
+  constructor(
+    private projectService: ProjectService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    
+    this.getProjectList();
+  }
+
+  getProjectList() {
+    this.showLoader = true;
+    let params = {
+      keyword: ''
+    }
+    this.projectService.getProjectList(params).subscribe((response) => {
+      this.projectList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectList = response?.data;
+       // this.totalRecords = response?.totalCount;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
 
 }
--- src/app/services/project-service/project.service.ts
+++ src/app/services/project-service/project.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
+
+export enum ProjectEndPoint {
+  PROJECT_LIST = '/project/list',
+  ADD_PROJECT = '/project/create',
+  PROJECT_EDIT = '/project/update',
+  DELETE_PROJECT = '/project/delete',
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  baseUrl!: string;
+
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+    this.baseUrl = environment.baseUrl;
+  }
+
+  getProjectList(payload: any): Observable<any> {
+    return this.httpClient
+      .get<any>(this.baseUrl + ProjectEndPoint.PROJECT_LIST, payload);
+  }
+
+  deleteProject(payload: any): Observable<any> {
+    return this.httpClient
+      .post<any>(this.baseUrl + ProjectEndPoint.DELETE_PROJECT, payload);
+  }
+
+  editProject(payload: any): Observable<any> {
+    return this.httpClient
+      .post<any>(this.baseUrl + ProjectEndPoint.PROJECT_EDIT, payload);
+  }
+
+  addProject(payload: any): Observable<any> {
+    return this.httpClient
+      .post<any>(this.baseUrl + ProjectEndPoint.ADD_PROJECT, payload);
+  }
+
+
+
 }
