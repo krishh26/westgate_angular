9018610518f582c2c0046404569cf387fd7611a3
kishanivotion <kishan@ivotiontech.com>
2024-04-22 16:47:07 +0530

    changes done

--- src/app/modules/bos-user/boss-user-add-project/boss-user-add-project.component.ts
+++ src/app/modules/bos-user/boss-user-add-project/boss-user-add-project.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./boss-user-add-project.component.scss']
 })
 export class BossUserAddProjectComponent {
+
+  systemCalendarFormat!: string;
+
+  ngOnInit(): void {
+    // Detect system calendar format
+    const date = new Date();
+    const format = new Intl.DateTimeFormat([], { calendar: 'auto' }).resolvedOptions().calendar;
+    this.systemCalendarFormat = format;
+
+    // You can set the initial value of the input field here
+    const formattedDate = formatDate(date, 'yyyy-MM-dd', 'en', format);
+    // Set formattedDate to your input field
+  }
 
 }
