d0148aa18f8ab751186c8df06985fd550e214f33
kishanivotion <kishan@ivotiontech.com>
2024-04-22 23:12:36 +0530

    changes done

--- src/app/modules/bos-user/fio-document-list/fio-document-list.component.ts
+++ src/app/modules/bos-user/fio-document-list/fio-document-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FoiService } from 'src/app/services/foi-service/foi.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ProjectService } from 'src/app/services/project-service/project.service';
 import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
 
 @Component({
@@ -17,9 +18,11 @@ export class FioDocumentListComponent {
   page: number = pagination.page;
   pagesize = pagination.itemsPerPage;
   totalRecords: number = pagination.totalRecords;
+  dateDifference: any
 
   constructor(
     private foiService: FoiService,
+    private projectService: ProjectService,
     private notificationService: NotificationService,
     private router: Router
   ) { }
@@ -28,27 +31,37 @@ export class FioDocumentListComponent {
     this.getFOIList();
   }
 
+  
+  formatMilliseconds(milliseconds: number): string {
+    const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    return `${days} days`;
+  }
+
+
   getFOIList() {
     this.showLoader = true;
-    this.foiService.getFOIList().subscribe((response) => {
+    let params = {
+      keyword: ''
+    }
+    this.projectService.getProjectList(params).subscribe((response) => {
       this.FOIList = [];
       this.totalRecords = 0;
       if (response?.status == true) {
         this.showLoader = false;
         this.FOIList = response?.data;
-        // this.projectList.forEach((project: any) => {
-        //   const dueDate = new Date(project.dueDate);
-        //   const currentDate = new Date();
-        //   const dateDifference = Math.abs(dueDate.getTime() - currentDate.getTime());
-        //   console.log(`Date difference for project ${dateDifference}`);
-        //   const formattedDateDifference: string = this.formatMilliseconds(dateDifference);
-        //   this.dateDifference = formattedDateDifference;
-        // });
+        this.FOIList.forEach((project: any) => {
+          const dueDate = new Date(project.dueDate);
+          const currentDate = new Date();
+          const dateDifference = Math.abs(dueDate.getTime() - currentDate.getTime());
+          console.log(`Date difference for project ${dateDifference}`);
+          const formattedDateDifference: string = this.formatMilliseconds(dateDifference);
+          this.dateDifference = formattedDateDifference;
+        });
       } else {
         this.notificationService.showError(response?.message);
         this.showLoader = false;
       }
-    }, (error: { message: string; }) => {
+    }, (error) => {
       this.notificationService.showError(error?.message);
       this.showLoader = false;
     });
