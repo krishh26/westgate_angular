ee7c6a7e7be5e84baec4030ecb1d35e8a09bf0a5
darshan <darshandumaraliya@gmail.com>
2024-05-05 00:01:06 +0530

    errror resolve.

--- src/app/utility/shared/components/uk-writer-header/uk-writer-header.component.ts
+++ src/app/utility/shared/components/uk-writer-header/uk-writer-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-uk-writer-header',
   templateUrl: './uk-writer-header.component.html',
-  styleUrls: ['./uk-writer-header.component.css']
+  styleUrls: ['./uk-writer-header.component.scss']
 })
 export class UkWriterHeaderComponent implements OnInit {
 
