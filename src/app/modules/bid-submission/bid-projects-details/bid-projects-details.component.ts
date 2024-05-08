52184587c6c634cda8080c0dc38a947508679073
kishanivotion <kishan@ivotiontech.com>
2024-05-09 04:13:01 +0530

    changes done

--- src/app/modules/bid-submission/bid-projects-details/bid-projects-details.component.ts
+++ src/app/modules/bid-submission/bid-projects-details/bid-projects-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ProjectService } from 'src/app/services/project-service/project.service';
 
 @Component({
   selector: 'app-bid-projects-details',
@@ -6,5 +9,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./bid-projects-details.component.scss']
 })
 export class BidProjectsDetailsComponent {
+
+  @ViewChild('downloadLink') private downloadLink!: ElementRef;
+
+  showLoader: boolean = false;
+  projectDetails: any = [];
+  projectId: string = '';
+  projectID: any;
+  dateDifference: any;
+  currentDate: Date = new Date();
+  selectedDocument : any;
+
+  constructor(
+    private projectService: ProjectService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      this.projectId = params['id']
+    });
+  }
+
+  ngOnInit(): void {
+    this.getProjectDetails();
+  }
+
+  formatMilliseconds(milliseconds: number): string {
+    const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    return `${days} days`;
+  }
+
+  getProjectDetails() {
+    this.showLoader = true;
+    this.projectService.getProjectDetailsById(this.projectId).subscribe((response) => {
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectDetails = response?.data;
+        const dueDate = new Date(this.projectDetails?.dueDate);
+        const currentDate = new Date();
+        const dateDifference = Math.abs(dueDate.getTime() - currentDate.getTime());
+        const formattedDateDifference: string = this.formatMilliseconds(dateDifference);
+        this.dateDifference = formattedDateDifference;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  openDocument(data: any) {
+    this.selectedDocument = data;
+  }
+
 
 }
