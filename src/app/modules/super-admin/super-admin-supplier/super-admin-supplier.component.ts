33ad0247ea1adb093eca86653caa59012f19b1ee
kishanivotion <kishan@ivotiontech.com>
2024-05-14 14:01:24 +0530

    changes done

--- src/app/modules/super-admin/super-admin-supplier/super-admin-supplier.component.ts
+++ src/app/modules/super-admin/super-admin-supplier/super-admin-supplier.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { SupplierAdminService } from 'src/app/services/supplier-admin/supplier-admin.service';
+import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
 
 @Component({
   selector: 'app-super-admin-supplier',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./super-admin-supplier.component.scss']
 })
 export class SuperAdminSupplierComponent {
+
+  showLoader: boolean = false;
+  manageUserList: any = [];
+
+  page: number = pagination.page;
+  pagesize = pagination.itemsPerPage;
+  totalRecords: number = pagination.totalRecords;
+
+  constructor(
+    private supplierService: SupplierAdminService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getManageUserList();
+  }
+
+  getManageUserList() {
+    this.showLoader = true;
+    this.supplierService.getManageUserList().subscribe((response) => {
+      this.manageUserList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.manageUserList = response?.data?.data;
+        this.totalRecords = response?.totalCount;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
 
 }
