55865ace51ee3249c0d81c1777b2a8b01e2000f8
kishanivotion <kishan@ivotiontech.com>
2024-05-14 12:16:55 +0530

    changes done

--- src/app/modules/super-admin/super-admin-dashboard/super-admin-dashboard.component.ts
+++ src/app/modules/super-admin/super-admin-dashboard/super-admin-dashboard.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { SuperadminService } from 'src/app/services/super-admin/superadmin.service';
 import { SupplierAdminService } from 'src/app/services/supplier-admin/supplier-admin.service';
-
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-super-admin-dashboard',
   templateUrl: './super-admin-dashboard.component.html',
@@ -14,6 +14,7 @@ export class SuperAdminDashboardComponent {
   superdashboardlist: any = [];
   superstatictics: any = [];
   showLoader: boolean = false;
+  chart: any = [];
 
   constructor(
     private superService: SuperadminService,
@@ -46,18 +47,49 @@ export class SuperAdminDashboardComponent {
 
   getSuperStatictics() {
     this.showLoader = true;
-    this.superService.getsuperstatictics().subscribe((response) => {
-      if (response?.status == true) {
-        this.showLoader = false;
-        this.superstatictics = response?.data;
-      } else {
-        this.notificationService.showError(response?.message);
+    this.superService.getsuperstatictics().subscribe(
+      (response) => {
+        if (response?.status == true) {
+          this.showLoader = false;
+          const responseData = response?.data;
+          this.superstatictics = Object.keys(responseData).map((date) => {
+            return {
+              day: responseData[date].day,
+              data: responseData[date].data
+            };
+          });
+          const day = this.superstatictics.map((project: any) => project?.day);
+          const data = this.superstatictics.map((project: any) => project?.data);
+          this.chart = new Chart('canvas', {
+            type: 'bar',
+            data: {
+              labels: day,
+              datasets: [
+                {
+                  data: data,
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          });
+        } else {
+          this.notificationService.showError(response?.message);
+          this.showLoader = false;
+        }
+      },
+      (error) => {
+        this.notificationService.showError(error?.message);
         this.showLoader = false;
       }
-    }, (error) => {
-      this.notificationService.showError(error?.message);
-      this.showLoader = false;
-    });
+    );
   }
+
 
 }
