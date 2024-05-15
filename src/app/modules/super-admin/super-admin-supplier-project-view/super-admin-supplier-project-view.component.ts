bd0473780d9ea948acf7b88b2e156baa4e6d1dea
kishanivotion <kishan@ivotiontech.com>
2024-05-15 11:28:49 +0530

    changes done

--- src/app/modules/super-admin/super-admin-supplier-project-view/super-admin-supplier-project-view.component.ts
+++ src/app/modules/super-admin/super-admin-supplier-project-view/super-admin-supplier-project-view.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ProjectService } from 'src/app/services/project-service/project.service';
+import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
+import { Payload } from 'src/app/utility/shared/constant/payload.const';
 
 @Component({
   selector: 'app-super-admin-supplier-project-view',
@@ -6,5 +12,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./super-admin-supplier-project-view.component.scss']
 })
 export class SuperAdminSupplierProjectViewComponent {
+
+  showLoader: boolean = false;
+  projectList: any = [];
+
+  page: number = pagination.page;
+  pagesize = pagination.itemsPerPage;
+  totalRecords: number = pagination.totalRecords;
+  dueDate: any;
+  currentDate: Date = new Date();
+  dateDifference: any;
+  searchText: any;
+  myControl = new FormControl();
+
+  constructor(
+    private projectService: ProjectService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.myControl.valueChanges.subscribe((res: any) => {
+      let storeTest = res;
+      this.searchText = res.toLowerCase();
+    });
+    this.getProjectList();
+  }
+
+  getProjectList() {
+    this.showLoader = true;
+    Payload.projectList.keyword = this.searchText;
+    Payload.projectList.page = String(this.page);
+    Payload.projectList.limit = String(this.pagesize);
+    this.projectService.getProjectList(Payload.projectList).subscribe((response) => {
+      this.projectList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectList = response?.data?.data;
+        console.log(this.projectList);
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  searchtext() {
+    this.showLoader = true;
+    Payload.projectList.keyword = this.searchText;
+    Payload.projectList.page = String(this.page);
+    Payload.projectList.limit = String(this.pagesize);
+    console.log(Payload.projectList);
+    this.projectService.getProjectList(Payload.projectList).subscribe((response) => {
+      this.projectList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectList = response?.data?.data;
+        console.log(this.projectList);
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  paginate(page: number) {
+    this.page = page;
+    this.getProjectList();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
