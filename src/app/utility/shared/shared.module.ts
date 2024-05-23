424ea042734f08f534a61b62662ae36d9c9d93f1
kishanivotion <kishan@ivotiontech.com>
2024-05-24 01:22:36 +0530

    changes doen

--- src/app/utility/shared/shared.module.ts
+++ src/app/utility/shared/shared.module.ts
@@ -6,15 +6,27 @@ import { ProjectManagerHeaderComponent } from './components/project-manager-head
 import { ProjectMangerHeaderTwoComponent } from './components/project-manger-header-two/project-manger-header-two.component';
 import { ProjectNotificationComponent } from './common/project-notification/project-notification.component';
 import { SuperadminHeaderComponent } from './components/superadmin-header/superadmin-header.component';
+import { NoRecordFoundComponent } from './common/no-record-found/no-record-found.component';
+import { CustomValidation } from './constant/custome-validation';
 
 @NgModule({
   declarations: [
+    NoRecordFoundComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoRecordFoundComponent,
+  ],
+  providers: [
+    CustomValidation
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
