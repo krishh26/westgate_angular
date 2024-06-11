d307ec5d8e92bd18bcedff19484b1719471be832
kishanivotion <kishan@ivotiontech.com>
2024-06-12 00:23:28 +0530

    changes done

--- src/app/modules/bos-user/boss-user-bulk-entry/boss-user-bulk-entry.component.ts
+++ src/app/modules/bos-user/boss-user-bulk-entry/boss-user-bulk-entry.component.ts
@@ -40,16 +40,45 @@ export class BossUserBulkEntryComponent {
       /* save data */
       let data = <any[][]>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
   
-      // Remove the first row (A1 row)
+      // Remove the first row (A1 row) which contains headers
+      const headers = data[0];
       data = data.slice(1);
   
       // Filter out empty arrays
       data = data.filter(row => row.length > 0);
   
-      console.log(data);
+      // Function to replace null or undefined values with empty strings
+      const replaceNullWithEmptyString = (value: any) => value == null ? "" : value;
+  
+      // Map the data to desired JSON format with null values replaced
+      const jsonData = data.map(row => {
+        return {
+          projectName: replaceNullWithEmptyString(row[0]),
+          category: replaceNullWithEmptyString(row[1]),
+          industry: replaceNullWithEmptyString(row[2]),
+          description: replaceNullWithEmptyString(row[3]),
+          BOSID: replaceNullWithEmptyString(row[4]),
+          publishDate: replaceNullWithEmptyString(row[5]),
+          submission: replaceNullWithEmptyString(row[6]),
+          link: replaceNullWithEmptyString(row[7]),
+          periodOfContractStart: replaceNullWithEmptyString(row[8]),
+          periodOfContractEnd: replaceNullWithEmptyString(row[9]),
+          dueDate: replaceNullWithEmptyString(row[10]),
+          value: replaceNullWithEmptyString(row[11]),
+          projectType: replaceNullWithEmptyString(row[12]),
+          website: replaceNullWithEmptyString(row[13]),
+          mailID: replaceNullWithEmptyString(row[14]),
+          clientType: replaceNullWithEmptyString(row[15]),
+          clientName: replaceNullWithEmptyString(row[16]),
+          noticeReference: replaceNullWithEmptyString(row[17]),
+          CPVCodes: replaceNullWithEmptyString(row[18])
+        };
+      });
+  
+      console.log(jsonData);
   
       // Now you can make your API call with the processed data
-      this.projectService.addProject(data).subscribe(
+      this.projectService.addProject(jsonData).subscribe(
         (response) => {
           if (response?.status == true) {
             this.showLoader = false;
@@ -68,6 +97,6 @@ export class BossUserBulkEntryComponent {
   
     reader.readAsBinaryString(target.files[0]);
   }
-
+  
 }
 
