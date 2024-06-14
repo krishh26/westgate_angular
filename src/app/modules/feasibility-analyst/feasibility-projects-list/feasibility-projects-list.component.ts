f1f4fe0f2a789b84c91f7d457498f531227b782d
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-06-15 02:45:16 +0530

    changes done

--- src/app/modules/feasibility-analyst/feasibility-projects-list/feasibility-projects-list.component.ts
+++ src/app/modules/feasibility-analyst/feasibility-projects-list/feasibility-projects-list.component.ts
@@ -220,4 +220,14 @@ export class FeasibilityProjectsListComponent {
     return foundIndustry ? foundIndustry.industry : '';
   }
 
+   // Number only validation
+   NumberOnly(event: any): boolean {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
+
 }
