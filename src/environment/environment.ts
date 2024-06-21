1be7abb4c345e3136405e8ea9f4ec5f7c7ac1df4
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-06-21 12:24:19 +0530

    changes done

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100',
-  baseUrl: 'https://20.197.44.87/api/v1'
+  baseUrl: 'http://20.197.44.87/api/v1'
 };
