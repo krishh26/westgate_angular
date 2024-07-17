4ead6e0dee6ad70d9db5a7658bb97b00e8afa52d
darshan <darshandumaraliya@gmail.com>
2024-07-18 03:00:48 +0530

    view document related changes.

--- src/app/modules/uk-writer/uk-writer-projects-details/uk-writer-projects-details.component.ts
+++ src/app/modules/uk-writer/uk-writer-projects-details/uk-writer-projects-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { ProjectService } from 'src/app/services/project-service/project.service';
@@ -10,28 +11,29 @@ import { UkWriterService } from 'src/app/services/uk-writer/uk-writer.service';
   styleUrls: ['./uk-writer-projects-details.component.scss']
 })
 export class UkWriterProjectsDetailsComponent {
-
-  projectID : string = '';
-  supplierId : string = '';
+  projectID: string = '';
+  supplierId: string = '';
   showLoader: boolean = false;
   projectDetails: any = [];
   supplierDetails: any = [];
   selectedDocument: any;
-  summaryquestionList :any
+  summaryquestionList: any;
+  uploadedDocument: any;
+  documents: any[] = [];
 
-
-  constructor(private route:ActivatedRoute,
-    private projectService:ProjectService,
+  constructor(private route: ActivatedRoute,
+    private projectService: ProjectService,
     private ukwriterService: UkWriterService,
     private router: Router,
     private notificationService: NotificationService,
-  ){
+    private sanitizer: DomSanitizer
+  ) {
 
     this.route.queryParams.subscribe((params) => {
-      if(params['id']){
+      if (params['id']) {
         this.projectID = params['id']
       }
-      if(params['supplierId']){
+      if (params['supplierId']) {
         this.supplierId = params['supplierId']
       }
     });
@@ -40,25 +42,40 @@ export class UkWriterProjectsDetailsComponent {
 
   }
 
-  getProjectDetails(){
-      this.projectService.getProjectDetailsById(this.projectID).subscribe((response)=>{
-        if (response?.status == true) {
-          this.showLoader = false;
-          this.projectDetails = response?.data;
+  getProjectDetails() {
+    this.projectService.getProjectDetailsById(this.projectID).subscribe((response) => {
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectDetails = response?.data;
+        this.processDocuments();
         this.summaryquestionList = response?.data?.summaryQuestion;
-        } else {
-          this.notificationService.showError(response?.message);
-          this.showLoader = false;
-        }
-      },
-      error=>{
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    },
+      error => {
         this.notificationService.showError(error?.message);
         this.showLoader = false;
       })
   }
 
-  getSupplierDetails(){
-    this.ukwriterService.getSupplierListById(this.projectID,this.supplierId).subscribe((response)=>{
+  processDocuments() {
+    if (this.projectDetails) {
+      this.documents = [
+        { label: 'Sub-contracting', file: this.projectDetails.subContractingfile },
+        { label: 'Economical partnership', file: this.projectDetails.economicalPartnershipQueryFile },
+        { label: 'Economical partnership response', file: this.projectDetails.economicalPartnershipResponceFile },
+        ...this.projectDetails.FeasibilityOtherDocuments.map((doc: any) => ({
+          label: doc.name || 'Other Document',
+          file: doc.file
+        }))
+      ];
+    }
+  }
+
+  getSupplierDetails() {
+    this.ukwriterService.getSupplierListById(this.projectID, this.supplierId).subscribe((response) => {
       if (response?.status == true) {
         this.showLoader = false;
         this.supplierDetails = response?.data;
@@ -67,19 +84,43 @@ export class UkWriterProjectsDetailsComponent {
         this.showLoader = false;
       }
     },
-    error=>{
-      this.notificationService.showError(error?.message);
-      this.showLoader = false;
-    })
+      error => {
+        this.notificationService.showError(error?.message);
+        this.showLoader = false;
+      })
   }
 
-  supplierListPage(){
-    localStorage.setItem('UKprojectID' ,this.projectID )
+  supplierListPage() {
+    localStorage.setItem('UKprojectID', this.projectID)
     this.router.navigate(['/uk-writer/uk-writer-supplier-list'], { queryParams: { id: this.projectID } });
   }
 
   openDocument(data: any) {
     this.selectedDocument = data;
+  }
+
+  openUploadedDocument(document: any) {
+    this.uploadedDocument = document;
+  }
+
+  isPdf(url: string): boolean {
+    return url?.endsWith('.pdf') || false;
+  }
+
+  isWordOrExcel(url: string): boolean {
+    return url?.endsWith('.doc') || url?.endsWith('.docx') || url?.endsWith('.xls') || url?.endsWith('.xlsx') || false;
+  }
+
+  isImage(url: string): boolean {
+    return url?.endsWith('.jpg') || url?.endsWith('.jpeg') || url?.endsWith('.png') || false;
+  }
+
+  getDocumentViewerUrl(url: string): SafeResourceUrl {
+    if (this.isWordOrExcel(url)) {
+      const officeUrl = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(url)}`;
+      return this.sanitizer.bypassSecurityTrustResourceUrl(officeUrl);
+    }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   download(imageUrl: string, fileName: string): void {
--- src/app/modules/uk-writer/uk-writer.module.ts
+++ src/app/modules/uk-writer/uk-writer.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UkWriterComponent } from './uk-writer.component';
 import { UkWriterHomeComponent } from './uk-writer-home/uk-writer-home.component';
@@ -37,6 +37,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
     NgbModule,
     UkWriterRoutingModule,
     NgSelectModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UkWriterModule { }
