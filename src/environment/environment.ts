33fe23d3a64fc713950e247d91641c5fe637dedc
Dhruv Suhagiya <81179798+Suhagiya-Dhruv@users.noreply.github.com>
2024-07-24 10:25:33 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100',
-  baseUrl: 'http://20.197.44.87/api/v1'
+  baseUrl: 'https://west-gate-it-hub-aan7.onrender.com/api/v1'
 };
