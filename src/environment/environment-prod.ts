533703e0f0e34a1fa099c991246c576c13ef4b0f
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-07-30 14:46:11 +0530

    changes done

--- src/environment/environment-prod.ts
+++ src/environment/environment-prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100',
-  baseUrl: 'https://west-gate-it-hub-aan7.onrender.com/api/v1'
+  baseUrl: 'http://194.238.23.184/api/v1/'
 };
