cd192efa1c8f217281e9c0695a609a524cf0c4e9
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-08-16 11:51:25 +0530

    api endpoiint add

--- src/environment/environment-prod.ts
+++ src/environment/environment-prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100',
-  baseUrl: 'http://194.238.23.184/api/v1'
+  baseUrl: 'https://api.westgateithub.in/api/v1'
 };
