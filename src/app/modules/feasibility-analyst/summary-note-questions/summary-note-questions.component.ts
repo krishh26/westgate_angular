8057fcf9d5e820c6f9eda94641ba9e3a9ebd8121
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-09-12 18:32:23 +0530

    changes done

--- src/app/modules/feasibility-analyst/summary-note-questions/summary-note-questions.component.ts
+++ src/app/modules/feasibility-analyst/summary-note-questions/summary-note-questions.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FeasibilityService } from 'src/app/services/feasibility-user/feasibility.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { ProjectService } from 'src/app/services/project-service/project.service';
 import { SummaryService } from 'src/app/services/summary/summary.service';
@@ -24,6 +25,8 @@ export class SummaryNoteQuestionsComponent {
   selectedDocument: any;
   isEditMode: boolean = false;
   currentSummaryId: number | null = null;
+  // uploaddocform: FormGroup | undefined;
+  showSuccess: boolean = false;
 
   summary = {
     questionName: new FormControl("", Validators.required),
@@ -37,7 +40,7 @@ export class SummaryNoteQuestionsComponent {
     // deadline: new FormControl("", Validators.required),
   }
 
-  summaryForm: FormGroup;
+  uploaddocform: FormGroup;
 
   constructor(
     private projectService: ProjectService,
@@ -45,13 +48,14 @@ export class SummaryNoteQuestionsComponent {
     private router: Router,
     private route: ActivatedRoute,
     private summaryService: SummaryService,
-
+    private feasibilityService: FeasibilityService,
   ) {
+    this.uploaddocform = new FormGroup(this.summary);
     this.route.queryParams.subscribe((params) => {
       this.projectId = params['id']
     });
-    this.summaryForm = new FormGroup(this.summary);
-    this.summaryForm.controls['projectId'].setValue(this.projectId)
+    // this.summaryForm = new FormGroup(this.summary);
+    // this.summaryForm.controls['projectId'].setValue(this.projectId)
   }
 
   ngOnInit(): void {
@@ -114,57 +118,25 @@ export class SummaryNoteQuestionsComponent {
   }
 
 
-  submitForm() {
-    this.summaryForm.markAllAsTouched();
-    if (this.summaryForm.invalid) {
-      return this.notificationService.showError('Please Fill All Summary Details.');
+  submitEligibilityForm() {
+    if (!this.uploaddocform.valid) {
+      return this.notificationService.showError('Please Fill Form.');
     }
+    this.feasibilityService.updateProjectDetails(this.uploaddocform.value, this.projectId).subscribe({
+      next: (res: any) => {
 
-
-    this.showLoader = true;
-    if (this.isEditMode && this.currentSummaryId !== null) {
-      // Update existing summary
-      this.summaryService.updateSummaryDetail(this.currentSummaryId.toString(), this.summaryForm.value).subscribe({
-        next: (response) => {
-          if (response?.status === true) {
-            this.showLoader = false;
-            this.getSummaryList();
-            this.resetForm();
-          } else {
-            this.notificationService.showError(response?.message);
-            this.showLoader = false;
-          }
-        },
-        error: (error) => {
-          this.notificationService.showError(error?.message);
-          this.showLoader = false;
-        }
-      });
-    } else {
-      // Add new summary
-      this.summaryService.addSummary(this.summaryForm.value).subscribe({
-        next: (response) => {
-          if (response?.status === true) {
-            this.showLoader = false;
-            this.getSummaryList();
-            this.resetForm();
-          } else {
-            this.notificationService.showError(response?.message);
-            this.showLoader = false;
-          }
-        },
-        error: (error) => {
-          this.notificationService.showError(error?.message);
-          this.showLoader = false;
-        }
-      });
-    }
+        // this.router.navigate(['/feasibility-user/feasibility-project-list']);
+      },
+      error: (err: any) => {
+        return this.notificationService.showError('Something went wrong');
+      }
+    })
   }
 
 
   resetForm() {
-    this.summaryForm.reset();
-    this.summaryForm.controls['projectId'].setValue(this.projectId)
+    this.uploaddocform.reset();
+    this.uploaddocform.controls['projectId'].setValue(this.projectId)
     this.isEditMode = false;
     this.currentSummaryId = null;
   }
@@ -172,7 +144,7 @@ export class SummaryNoteQuestionsComponent {
   editSummary(summary: any) {
     this.isEditMode = true;
     this.currentSummaryId = summary?._id;
-    this.summaryForm.patchValue({
+    this.uploaddocform.patchValue({
       summaryQuestionFor: summary.summaryQuestionFor,
       questionName: summary.questionName,
       question: summary.question,
