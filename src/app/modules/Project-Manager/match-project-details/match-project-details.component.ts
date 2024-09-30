a76a98d597834f8fe4300ff1ee88b363c61499ac
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-01 00:31:52 +0530

    changes done

--- src/app/modules/Project-Manager/match-project-details/match-project-details.component.ts
+++ src/app/modules/Project-Manager/match-project-details/match-project-details.component.ts
@@ -1,11 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { ProjectService } from 'src/app/services/project-service/project.service';
-import { formatDate } from '@angular/common';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ProjectManagerService } from 'src/app/services/project-manager/project-manager.service';
+import { SummaryService } from 'src/app/services/summary/summary.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-match-project-details',
@@ -14,36 +13,29 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class MatchProjectDetailsComponent {
 
+ 
   @ViewChild('downloadLink') private downloadLink!: ElementRef;
 
   showLoader: boolean = false;
   projectDetails: any = [];
   projectId: string = '';
+  projectID: any;
   dateDifference: any;
   currentDate: Date = new Date();
   selectedDocument: any;
   loginUser: any;
-  summaryQuestionList: any
-  userDetail: any
-  selectedSuppliers: { [key: string]: { company: string; startDate: any } } = {};
-  myForm: FormGroup | undefined;
-  skills: any;
-  selectedSupplier: any;
-  casestudylist : any =[];
-  // skills: FormArray;
-  companyDetails: any = [
-    {
-      name: "Delphi Services Limited"
-    }, {
-      name: "Spectrum IT Hub Limited"
-    }, {
-      name: "Apex IT Solutions"
-    }, {
-      name: "Big Data Limited"
-    }, {
-      name: "Saiwen"
-    }
-  ]
+  summaryQuestionList: any;
+  selectedDate: any;
+  summaryId: any;
+  statusList: string[] = [
+    'In solution',
+    'In-review',
+    'In-Submission',
+    'Submitted',
+    'Awarded',
+    'Not awarded',
+    'Dropped'
+  ];
 
   constructor(
     private projectService: ProjectService,
@@ -51,8 +43,8 @@ export class MatchProjectDetailsComponent {
     private router: Router,
     private route: ActivatedRoute,
     private localStorageService: LocalStorageService,
-    private fb: FormBuilder,
-    private projectManagerService: ProjectManagerService,
+    private summaryService: SummaryService,
+    private datePipe: DatePipe,
     private sanitizer: DomSanitizer
   ) {
     this.route.queryParams.subscribe((params) => {
@@ -65,11 +57,6 @@ export class MatchProjectDetailsComponent {
   ngOnInit(): void {
     this.getProjectDetails();
     this.getSummaryQuestion();
-    this.getUserDetails()
-    this.myForm = this.fb.group({
-      skills: this.fb.array([])
-    });
-    this.skills = this.myForm.get('skills') as FormArray;
   }
 
   getProjectDetails() {
@@ -78,7 +65,6 @@ export class MatchProjectDetailsComponent {
       if (response?.status == true) {
         this.showLoader = false;
         this.projectDetails = response?.data;
-        this.casestudylist = response?.data?.casestudy;
       } else {
         this.notificationService.showError(response?.message);
         this.showLoader = false;
@@ -87,50 +73,6 @@ export class MatchProjectDetailsComponent {
       this.notificationService.showError(error?.message);
       this.showLoader = false;
     });
-  }
-
-  getUserDetails() {
-    this.projectManagerService.getUserList('SupplierAdmin').subscribe((response) => {
-      if (response?.status == true) {
-        this.showLoader = false;
-        this.userDetail = response?.data;
-        this.selectedSuppliers = this.userDetail.reduce((acc: any, supplier: any) => {
-          acc[supplier._id] = { company: '', startDate: null };
-          return acc;
-        }, {});
-      } else {
-        this.notificationService.showError(response?.message);
-        this.showLoader = false;
-      }
-    }, (error) => {
-      this.notificationService.showError(error?.message);
-      this.showLoader = false;
-    });
-  }
-
-  dropUser(details: any) {
-    console.log('this is testing data', details);
-    if (!details?.reason) {
-      return this.notificationService.showError('Please enter reason');
-    }
-
-    const data = {
-      dropUser: {
-        userId: details?._id,
-        reason: details?.reason
-      }
-    }
-
-    this.projectManagerService.dropUser(data, this.projectId).subscribe((response) => {
-      if (response?.status == true) {
-        this.notificationService.showSuccess(response?.message || 'Drop user successfully');
-        this.getUserDetails();
-      } else {
-        return this.notificationService.showError('Try after some time.');
-      }
-    }, (error) => {
-      this.notificationService.showError(error?.message || 'Error.')
-    })
   }
 
   getSummaryQuestion() {
@@ -167,74 +109,63 @@ export class MatchProjectDetailsComponent {
       });
   }
 
-  getDate(date: any) {
-    let formattedDate = formatDate(date, 'yyyy-MM-dd', 'en-US');
-    return formattedDate
+  editSummaryDate(date: any, summaryId: string) {
+    this.summaryId = summaryId
+    this.selectedDate = this.datePipe.transform(date, 'yyyy-MM-dd', 'en-US');
   }
 
-  addSupplier(supplierId: string) {
-    const selectedCompany = this.selectedSuppliers[supplierId]?.company;
-    const selectedStartDate = this.selectedSuppliers[supplierId]?.startDate;
-    if (!selectedCompany) {
-      alert('Please select a company');
-      return;
+  editUserName() {
+    console.log('editUserName :');
+  }
+  saveDeadLineDate() {
+    let payload = {
+      deadline: this.selectedDate
     }
-
-    if (!selectedStartDate) {
-      alert('Please select a start date');
-      return;
-    }
-    console.log(`Adding supplier ${supplierId} with company: ${selectedCompany}, start date: ${selectedStartDate}`);
-    let reqPayload = {
-      select: { supplierId: supplierId, handoverCall: selectedStartDate, companySelect: [selectedCompany] }
-    }
-    this.projectService.addSupplier(this.projectId, reqPayload).subscribe({
-      next: (res: any) => {
-        console.log('reqPayload :', reqPayload);
-
+    this.summaryService.updateSummaryDetail(this.summaryId, payload).subscribe((response) => {
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.getSummaryQuestion()
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
       }
-    })
-  }
-
-  addSkill() {
-    this.skills?.push(this.fb.control(''));
-  }
-
-  removeSkill(index: number) {
-    this.skills.removeAt(index);
-  }
-
-  getSkill(index: number) {
-    return this.skills.at(index) as FormControl;
-  }
-
-  goToSummaryDetails() {
-    this.router.navigate(['/project-manager/project/summary-notes'], { queryParams: { id: this.projectId } });
-  }
-
-  selectSupplier(supplier: any) {
-    this.selectedSupplier = supplier
-
-    if (!this.selectedSupplier) {
-      return this.notificationService.showError('please select supplier');
-    }
-    console.log('sadsdd',supplier);
-
-    const data = {
-      select: {
-        supplierId: this.selectedSupplier?._id,
-        companySelect: supplier.company,
-        handoverCall: supplier.startDate
-      }
-    }
-    this.projectManagerService.dropUser(data, this.projectId).subscribe((response) => {
-      this.notificationService.showSuccess('Successfully select user')
     }, (error) => {
-      this.notificationService.showError(error?.message || 'Something went wrong');
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
     });
   }
 
-  
+  onStatusChange(supplier: any) {
+    const payload = {
+      projectId: this.projectDetails?._id,
+      supplierId: supplier?.supplierId,
+      supplierStatus: supplier?.supplierDetails?.status
+    };
+
+    this.changeSupplierStatus(payload);
+  }
+
+  changeSupplierStatus(payload: any) {
+    this.projectService.changeProjectSupplierStatus(payload).subscribe((response) => {
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.notificationService.showSuccess(response?.message);
+        this.getSummaryQuestion();
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  openSummaryDetail(item: any) {
+    localStorage.setItem('ViewSummary', JSON.stringify(item));
+    this.router.navigate(['/project-manager/project/summary-project-details']);
+  }
+
   isPdf(url: string): boolean {
     return url?.endsWith('.pdf') || false;
   }
