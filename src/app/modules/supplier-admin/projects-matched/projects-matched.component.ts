ae34d9070cc001361917ab760bc7e4ca22678fe6
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-07 02:36:34 +0530

    changes done

--- src/app/modules/supplier-admin/projects-matched/projects-matched.component.ts
+++ src/app/modules/supplier-admin/projects-matched/projects-matched.component.ts
@@ -1,3 +1,4 @@
+import { Options } from '@angular-slider/ngx-slider';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -5,6 +6,7 @@ import { debounceTime } from 'rxjs';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { ProjectService } from 'src/app/services/project-service/project.service';
+import { SuperadminService } from 'src/app/services/super-admin/superadmin.service';
 import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
 import { Payload } from 'src/app/utility/shared/constant/payload.const';
 
@@ -19,25 +21,178 @@ export class ProjectsMatchedComponent implements OnInit {
   page: number = pagination.page;
   pagesize = pagination.itemsPerPage;
   totalRecords: number = pagination.totalRecords;
-  searchText: FormControl = new FormControl('');
+  searchText: any;
   loginUser: any;
 
+  minValue: number = 0;
+  maxValue: number = 50000000;
+  options: Options = {
+    floor: 0,
+    ceil: 50000000
+  };
+
+  selectedCategories: any[] = [];
+  selectedIndustries: any[] = [];
+  selectedProjectTypes: any[] = [];
+  selectedClientTypes: any[] = [];
+  selectedStatuses: any[] = [];
+
+  projectTypeList = [
+    { projectType: 'Development', value: 'Development' },
+    { projectType: 'Product', value: 'Product' },
+    { projectType: 'Service', value: 'Service' }
+  ];
+
+  clientTypeList = [
+    { clientType: 'Public Sector', value: 'PublicSector' },
+    { clientType: 'Private Sector', value: 'PrivateSector' }
+  ];
+
+  statusList = [
+    { value: 'Awaiting', status: 'Awaiting' },
+    { value: 'InProgress', status: 'In-Progress' },
+    { value: 'InHold', status: 'In Hold' },
+    { value: 'Pass', status: 'Pass' },
+    { value: 'Fail', status: 'Fail' },
+
+    { value: 'InSolution', status: 'InSolution' },
+    { value: 'InReview', status: 'InReview' },
+    { value: 'Submitted', status: 'Submitted' },
+    { value: 'InSubmission', status: 'InSubmission' },
+    { value: 'Awarded', status: 'Awarded' },
+    { value: 'NotAwarded', status: 'NotAwarded' },
+
+  ];
+  categoryList: any = [];
+  industryList: any = [];
+  myControl = new FormControl();
+  publishStartDate: FormControl = new FormControl('');
+  publishEndDate: FormControl = new FormControl('');
+  submissionStartDate: FormControl = new FormControl('');
+  submissionEndDate: FormControl = new FormControl('');
+  dateDifference: any;
   constructor(
     private projectService: ProjectService,
     private notificationService: NotificationService,
     private router: Router,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private superService: SuperadminService
   ) {
     this.loginUser = this.localStorageService.getLogger();
   }
 
   ngOnInit(): void {
-    this.searchText.valueChanges.pipe(debounceTime(500)).subscribe((result) => {
-      console.log('result', result);
-      this.getProjectList();
+    this.myControl.valueChanges.subscribe((res: any) => {
+      let storeTest = res;
+      this.searchText = res.toLowerCase();
     });
-
+    this.getcategoryList();
+    this.getIndustryList();
     this.getProjectList();
+    this.publishEndDate.valueChanges.subscribe((res: any) => {
+      if (!this.publishStartDate.value) {
+        this.notificationService.showError('Please select a Publish start date');
+        return
+      } else {
+        this.searchtext()
+      }
+    });
+    this.submissionEndDate.valueChanges?.subscribe((res: any) => {
+      if (!this.submissionStartDate.value) {
+        this.notificationService.showError('Please select a Submission start date');
+        return
+      } else {
+        this.searchtext()
+      }
+    });
+  }
+
+
+  changeRange() {
+    if (this.maxValue >= this.minValue) {
+      this.searchtext();
+    }
+  }
+
+
+  searchtext() {
+    this.showLoader = true;
+    // Update payload with filters
+    Payload.projectList.keyword = this.searchText;
+    Payload.projectList.page = String(this.page);
+    Payload.projectList.limit = String(this.pagesize);
+    Payload.projectList.category = this.selectedCategories.join(',');
+    Payload.projectList.industry = this.selectedIndustries.join(',');
+    Payload.projectList.projectType = this.selectedProjectTypes.join(',');
+    Payload.projectList.clientType = this.selectedClientTypes.join(',');
+    Payload.projectList.status = this.selectedStatuses.join(',')
+    Payload.projectList.publishDateRange = (this.publishStartDate.value && this.publishEndDate.value) ? `${this.publishStartDate.value.year}-${this.publishStartDate.value.month}-${this.publishStartDate.value.day} , ${this.publishEndDate.value.year}-${this.publishEndDate.value.month}-${this.publishEndDate.value.day}` : '';
+    Payload.projectList.SubmissionDueDateRange = (this.submissionStartDate.value && this.submissionEndDate.value) ? `${this.submissionStartDate.value.year}-${this.submissionStartDate.value.month}-${this.submissionStartDate.value.day} , ${this.submissionEndDate.value.year}-${this.submissionEndDate.value.month}-${this.submissionEndDate.value.day}` : '';
+    Payload.projectList.valueRange = this.minValue + '-' + this.maxValue;
+
+    this.projectService.getProjectList(Payload.projectList).subscribe((response) => {
+      this.projectList = [];
+      this.totalRecords = response?.data?.meta_data?.items;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectList = response?.data?.data;
+        console.log(this.projectList);
+
+        this.projectList.forEach((project: any) => {
+          const dueDate = new Date(project.dueDate);
+          const currentDate = new Date();
+          const dateDifference = Math.abs(dueDate.getTime() - currentDate.getTime());
+
+          const formattedDateDifference: string = this.formatMilliseconds(dateDifference);
+          this.dateDifference = formattedDateDifference;
+        });
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  formatMilliseconds(milliseconds: number): string {
+    const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    return `${days} days`;
+  }
+
+
+  getcategoryList() {
+    this.showLoader = true;
+    this.superService.getCategoryList().subscribe((response) => {
+      if (response?.message == "category fetched successfully") {
+        this.showLoader = false;
+        this.categoryList = response?.data;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  getIndustryList() {
+    this.showLoader = true;
+    this.superService.getIndustryList().subscribe((response) => {
+      if (response?.message == "Industry fetched successfully") {
+        this.showLoader = false;
+        this.industryList = response?.data;
+        console.log(this.industryList);
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
   }
 
   getProjectList() {
@@ -54,7 +209,7 @@ export class ProjectsMatchedComponent implements OnInit {
       if (response?.status == true) {
         this.showLoader = false;
         this.projectList = response?.data?.data;
-      
+
       } else {
         this.notificationService.showError(response?.message);
         this.showLoader = false;
@@ -83,7 +238,7 @@ export class ProjectsMatchedComponent implements OnInit {
   }
 
   projectDetails(projectId: any) {
-    this.router.navigate(['/supplier-admin/projects-details'], { queryParams: { id: projectId , type : 1 } });
+    this.router.navigate(['/supplier-admin/projects-details'], { queryParams: { id: projectId, type: 1 } });
   }
 
   paginate(page: number) {
