3ec8d4a69d818276bd62280dbf7d38d75761bc4c
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-12 21:21:22 +0530

    changes done

--- src/app/modules/super-admin/super-admin-dashboard/super-admin-dashboard.component.ts
+++ src/app/modules/super-admin/super-admin-dashboard/super-admin-dashboard.component.ts
@@ -4,6 +4,8 @@ import { NotificationService } from 'src/app/services/notification/notification.
 import { SuperadminService } from 'src/app/services/super-admin/superadmin.service';
 import { SupplierAdminService } from 'src/app/services/supplier-admin/supplier-admin.service';
 import Chart from 'chart.js/auto';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { pagination } from 'src/app/utility/shared/constant/pagination.constant';
 @Component({
   selector: 'app-super-admin-dashboard',
   templateUrl: './super-admin-dashboard.component.html',
@@ -13,28 +15,34 @@ export class SuperAdminDashboardComponent {
 
   superdashboardlist: any = [];
   superstatictics: any = [];
+  supplierUserList: any = [];
   showLoader: boolean = false;
   chart: any = [];
   selectedDuration: string = 'daily'; // Default value
+  page: number = pagination.page;
+  pagesize = pagination.itemsPerPage;
+  totalRecords: number = pagination.totalRecords;
+
   constructor(
     private superService: SuperadminService,
     private notificationService: NotificationService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authservice: AuthService,
   ) {
   }
 
   ngOnInit(): void {
     this.getProjectDetails('daily');
     this.getSuperStatictics();
+    this.getManageUserList();
   }
 
   onDurationChange(duration: 'yearly' | 'monthly' | 'weekly' | 'daily') {
-    this.selectedDuration = duration;  // Update the selectedDuration with the chosen value
-    this.getProjectDetails(duration);  // Now call getProjectDetails with the updated duration
+    this.selectedDuration = duration;
+    this.getProjectDetails(duration);
   }
 
-  // Pass selectedDuration in the API call
   getProjectDetails(duration: 'yearly' | 'monthly' | 'weekly' | 'daily') {
     this.showLoader = true;
     this.superService.getDashboardList({ duration: this.selectedDuration }).subscribe((response) => {
@@ -98,5 +106,26 @@ export class SuperAdminDashboardComponent {
     );
   }
 
+  getManageUserList() {
+    this.showLoader = true;
+    this.authservice.getUserList('SupplierAdmin').subscribe(
+      (response) => {
+        this.supplierUserList = [];
+        this.totalRecords = response?.data?.meta_data?.items;
+        if (response?.status == true) {
+          this.showLoader = false;
+          this.supplierUserList = response?.data;
+          this.totalRecords = response?.totalCount;
+        } else {
+          this.notificationService.showError(response?.message);
+          this.showLoader = false;
+        }
+      },
+      (error) => {
+        this.notificationService.showError(error?.message);
+        this.showLoader = false;
+      }
+    );
+  }
 
 }
