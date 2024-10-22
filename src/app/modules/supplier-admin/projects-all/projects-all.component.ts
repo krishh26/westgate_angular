8f4fadf50b2881db9176d6d00aed33012c271bf3
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-22 15:29:57 +0530

    changes done

--- src/app/modules/supplier-admin/projects-all/projects-all.component.ts
+++ src/app/modules/supplier-admin/projects-all/projects-all.component.ts
@@ -51,11 +51,11 @@ export class ProjectsAllComponent implements OnInit {
   ];
 
   statusList = [
-    { value: 'Awaiting', status: 'Awaiting' },
-    { value: 'InProgress', status: 'In-Progress' },
-    { value: 'InHold', status: 'In Hold' },
-    { value: 'Passed', status: 'Pass' },
-    { value: 'Fail', status: 'Fail' },
+    // { value: 'Awaiting', status: 'Awaiting' },
+    // { value: 'InProgress', status: 'In-Progress' },
+    // { value: 'InHold', status: 'In Hold' },
+    // { value: 'Passed', status: 'Pass' },
+    // { value: 'Fail', status: 'Fail' },
 
     { value: 'InSolution', status: 'InSolution' },
     { value: 'InReview', status: 'InReview' },
--- src/app/modules/supplier-admin/supplier-project-work-in-progress/supplier-project-work-in-progress.component.ts
+++ src/app/modules/supplier-admin/supplier-project-work-in-progress/supplier-project-work-in-progress.component.ts
@@ -48,11 +48,11 @@ export class SupplierProjectWorkInProgressComponent {
 
   isExpired: boolean = false;
   statusList = [
-    { value: 'Awaiting', status: 'Awaiting' },
-    { value: 'InProgress', status: 'In-Progress' },
-    { value: 'InHold', status: 'In Hold' },
-    { value: 'Passed', status: 'Pass' },
-    { value: 'Fail', status: 'Fail' },
+    // { value: 'Awaiting', status: 'Awaiting' },
+    // { value: 'InProgress', status: 'In-Progress' },
+    // { value: 'InHold', status: 'In Hold' },
+    // { value: 'Passed', status: 'Pass' },
+    // { value: 'Fail', status: 'Fail' },
 
     { value: 'InSolution', status: 'InSolution' },
     { value: 'InReview', status: 'InReview' },
