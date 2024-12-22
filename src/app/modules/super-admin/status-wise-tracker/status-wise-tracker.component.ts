24e78e16c282f95606537c82a3de4c0f1a4a6d79
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-12-23 01:12:22 +0530

    changes done project details page

--- src/app/modules/super-admin/status-wise-tracker/status-wise-tracker.component.ts
+++ src/app/modules/super-admin/status-wise-tracker/status-wise-tracker.component.ts
@@ -154,7 +154,7 @@ export class StatusWiseTrackerComponent implements OnInit {
   };
 
   projectDetails(projectId: any) {
-    this.router.navigate(['/super-admin/super-admin-project-details'], { queryParams: { id: projectId } });
+    this.router.navigate(['/super-admin/tracker-wise-project-details'], { queryParams: { id: projectId } });
   }
 
   paginate(page: number) {
