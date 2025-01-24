860dfa20fffb33fd69236955eecf9d4c16e4513a
Kishansinh Parmar <kishansinhp26@gmail.com>
2025-01-25 01:27:37 +0530

    changes done

--- src/app/modules/Project-Manager/project-manager-project-details/project-manager-project-details.component.ts
+++ src/app/modules/Project-Manager/project-manager-project-details/project-manager-project-details.component.ts
@@ -428,9 +428,9 @@ export class ProjectManagerProjectDetailsComponent {
   }
 
   summaryDetail(type: string) {
-    if (!this.projectDetails?.clientDocument.length) {
-      return this.notificationService.showError('Upload Client Document');
-    }
+    // if (!this.projectDetails?.clientDocument.length) {
+    //   return this.notificationService.showError('Upload Client Document');
+    // }
     // if (!this.projectDetails?.loginDetail.length) {
     //   return this.notificationService.showError('Upload Login Detail');
     // }
