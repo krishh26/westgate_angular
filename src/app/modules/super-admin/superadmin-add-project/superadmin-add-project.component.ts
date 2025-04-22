316ed87473e65cbc0bb192783094d7469ee7ebcd
26krishh <kishansinhparmar999@gmail.com>
2025-04-22 15:42:05 +0530

    changes done

--- src/app/modules/super-admin/superadmin-add-project/superadmin-add-project.component.ts
+++ src/app/modules/super-admin/superadmin-add-project/superadmin-add-project.component.ts
@@ -39,7 +39,7 @@ export class SuperadminAddProjectComponent implements OnInit {
     CPVCodes: new FormControl(""),
     minValue: new FormControl(""),
     maxValue: new FormControl(""),
-    bidsubmissiontime: new FormControl("12"),
+    bidsubmissiontime: new FormControl(""),
     documentsLink: new FormControl(""),
     linkToPortal: new FormControl(""),
     password: new FormControl(""),
