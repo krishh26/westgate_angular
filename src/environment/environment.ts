43b3047cb4c8353dd48348921ad47f277151cb2d
darshan <darshandumaraliya@gmail.com>
2025-05-07 22:11:56 +0530

    Change env backend url.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
 
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100' ,
-  baseUrl: 'https://api.westgateithub.in/api/v1'
+  // baseUrl: 'https://api.westgateithub.in/api/v1'
+  baseUrl: 'https://api.westgateithub.com/api/v1'
 };
